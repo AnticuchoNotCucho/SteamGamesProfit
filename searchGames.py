eadea114b4ba586de87a80cf0eeb6ae937b0f762
Anticucho <jorge_2jul@hotmail.com>
2022-06-26 20:45:13 -0400

    Parcialmente funcionando, faltan procesos de comparacion de precios

--- searchGames.py
+++ searchGames.py
@@ -1,77 +1,120 @@
+import json
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import sys 
+import sys
 import pandas as pd
-import re 
+import re
+import requests
+import numpy as np
 
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-appid = '1334590'
-url = "https://www.steamcardexchange.net/index.php?gamepage-appid-"+appid
-driver.get(url)
-page = BeautifulSoup(driver.page_source, 'html.parser')
-first_row = page.find('div', class_='showcase-element-container card')
-second_row = first_row.find_next ('div', class_='showcase-element-container card')
-third_row = second_row.find_next ('div', class_='showcase-element-container card')
-fourth_row = third_row.find_next ('div', class_='showcase-element-container card')
-amount_cards = first_row.find('span', class_='element-count')
-amount_cards = amount_cards.text
-amount_cards = amount_cards.split()
-amount_cards = int(amount_cards[3])
-print(amount_cards)
-if amount_cards > 5:
-    for cards in first_row:
-        title = cards.find('span', class_='element-text')
-        price = cards.find('a', class_='button-blue')
-        if not title:
-            print('No Card')
-        else:
-            print(title.text)
-            print(price.text)
-            
-    for cards in second_row:
-        title = cards.find('span', class_='element-text')
-        if not title:
-            print('No Card')
-        else:
-            print(title.text)
-            print(price.text)
-# if amount_cards <= 5 :
-#     for cards in first_row:
-#         title = cards.find('span', class_='element-text')
-#     if not title:
-#         print('No Card')
-#     else:
-#         print(title.text,' ')
-#         print(cards.text)
-# elif amount_cards > 5:
-#     for cards in first_row:
-#         title = cards.find('span', class_='element-text')
-#     if not title:
-#         print('No Card')
-#     else:
-#         print(title.text,' ')
-#         print(cards.text)
-#     for cards in second_row:
-#         title = cards.find('span', class_='element-text')
-#     if not title:
-#         print('No Card')
-#     else:
-#         print(title.text,' ')
-#         print(cards.text)
+games_json = json.load(open('games.json'))
+mean_values = []
 
-# print(divs.text)
+for game in games_json:
+    price_list = []
+    appid = game['AppID']
+    url = "https://www.steamcardexchange.net/index.php?gamepage-appid-"+appid
+    driver = requests.get(url)
+    page = BeautifulSoup(driver.text, 'html.parser')
+    if page.find('div', class_="game-title"):
+        first_row = page.find('div', class_='showcase-element-container card')
+        second_row = first_row.find_next(
+            'div', class_='showcase-element-container card')
+        third_row = second_row.find_next(
+            'div', class_='showcase-element-container card')
+        amount_cards = first_row.find('span', class_='element-count')
+        amount_cards = amount_cards.text
+        amount_cards = amount_cards.split()
+        amount_cards = int(amount_cards[3])
+        print(game['Title'], game['AppID'], amount_cards)
+        if amount_cards <= 6:
+            for cards in first_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
 
-# for div in divs:
-#     title = div.find('span', class_='element-text')
-#     if not title:
-#         print('No Card')
-#     else:
-#         print(title.text,' ')
-#         print(div.text)
+        if amount_cards > 6 and amount_cards <= 12:
 
+            second_row = first_row.find_next(
+                'div', class_='showcase-element-container card')
+
+            for cards in first_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
+
+            for cards in second_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
+
+        elif amount_cards > 12 and amount_cards <= 18:
+            second_row = first_row.find_next(
+                'div', class_='showcase-element-container card')
+            third_row = second_row.find_next(
+                'div', class_='showcase-element-container card')
+            for cards in first_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
+            for cards in second_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
+
+            for cards in third_row:
+                title = cards.find('span', class_='element-text')
+                price = cards.find('a', class_='button-blue')
+                if not title:
+                    break
+                else:
+                    price = price.text
+                    price = re.findall(r'[\d\.\d]+', price)
+                    price = price[0]
+                    price_list.append((price))
+        array = np.array(price_list)
+        array = array.astype(float)
+        print(array)
+        mean = np.mean(array)*(amount_cards/2)
+        mean = mean * 124
+        profit = game['Price']
+        mean_values.append((game['Title'],np.round(mean, 2), game['Price'],game['discount']))
+    else: 
+        mean_values.append((game['Title'], "No info"))
+
+df = pd.DataFrame(mean_values, columns=['Game','MeanValue','GamePrice','MeanProfit'])
+df.to_json('Values.json', orient='records', indent=2)
+print(df)
---
+++ searchwithrequest.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import sys 
+import pandas as pd
+import re 
+import requests
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+
+list_pages = ['https://store.steampowered.com/search/?sort_by=Price_ASC&maxprice=70&category1=998%2C996&category2=29&os=win'
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=2',
+              'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=3',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=4',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=5',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=6',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=7',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=8',
+               'https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=9',
+               ]
+              
+games = []
+for page in list_pages:
+    driver = requests.get(page)
+    page = BeautifulSoup(driver.text, 'html.parser')
+    links = page.findAll('a', class_='search_result_row ds_collapse_flag app_impression_tracked')
+    pages = page.findAll('a')
+    
+    for page in pages:
+        if page.get('href') is not None:
+            if 'store.steampowered.com/app/' in page.get('href'):
+                appid = page.get('data-ds-appid')
+                title = page.find('span', class_='title')
+                div = page.find('div' , class_='col search_price discounted responsive_secondrow')
+                discount = page.find('div', class_="col search_discount responsive_secondrow")
+                discount = (discount.text)
+                discount = re.sub('[^0-9]', '', discount)
+                div = (div.text).split()
+                if "," in appid:
+                    games.append((title.text, appid + "es un pack", float(div[2].replace(',', '.')),discount))
+                else:
+                    games.append((title.text, appid, float(div[2].replace(',', '.')),discount))
+                
+print(len(games))
+df = pd.DataFrame(games, columns=['Title', 'AppID', 'Price', 'Discount'])
+print(df)    
+df.to_json('games.json', orient='records', indent=4)
+
+
+
+
+#href="https://store.steampowered.com/search/?sort_by=Price_ASC&sort_order=ASC&category1=998%2C996&category2=29&price=%2C70&os=win&maxprice=70&page=2"
+
